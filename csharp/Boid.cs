cca04c59268cd2a89df5e2abb154eddfec3f7240
Albert Glendinning <albertglend@gmail.com>
2022-11-29 23:56:02 +0000

    Almost fully working boids Program

--- csharp/Boid.cs
+++ csharp/Boid.cs
@@ -8,140 +8,190 @@ using OpenTK.Graphics.OpenGL4;
 
 namespace Boids
 {
-   class Boid
-   {
-       private float maxVelocity = 100.0f;
-       
-       private Vector2 velocity;
-       private float angle = (float)Math.PI /2 ;
-       private float time;
-       private Vector2 position;
-       private Vector2 WindowSize;
-       private Vector2 size = new Vector2(5, 10);
-       private Random rnd;
-       private static int VAO;
-       
-       private static float VisualRange = 150.0f;
+    class Boid
+    {
+        private float maxVelocity = 1.0f;
 
-    public Vector2 Position{get => position;}
-       public Boid()
-       {
-        this.rnd = new Random();
-       }
-       public Boid(Vector2 WindowSize, Random rnd)
-       {
-           this.WindowSize = WindowSize;
-           this.rnd = rnd;
-           Reset();
+        private Vector2 velocity;
 
-       }
-       public static void Setup()
-       {
-           //vertices of the droplet: triangle
-           float[] Vertices = new float[]
-           {
+        private float time;
+        private Vector2 position;
+        private Vector2 WindowSize;
+        private Vector2 size = new Vector2(5, 10);
+        private Random rnd;
+        private static int VAO;
+
+        private static float VisualRange = 60.0f;
+        private static float SeparationInput = 40;
+        public Vector2 Position { get => position; }
+        public Boid()
+        {
+            this.rnd = new Random();
+        }
+        public Boid(Vector2 WindowSize, Random rnd)
+        {
+            this.WindowSize = WindowSize;
+            this.rnd = rnd;
+            Reset();
+
+        }
+        public static void Setup()
+        {
+            //vertices of the droplet: triangle
+            float[] Vertices = new float[]
+            {
            -0.8f,-0.6f,
            0f,0.6f,
            0.8f,-0.6f,
-           };
-           //index of the vertices for constructing triangle
-           uint[] Indices = new uint[]
-           {
-           0,1,2,
-           };
-
-           VAO = GL.GenVertexArray();
-           int VBO = GL.GenBuffer();
-           int EBO = GL.GenBuffer();
-
-           GL.BindVertexArray(VAO);
-           GL.BindBuffer(BufferTarget.ArrayBuffer,VBO);
-           GL.BindBuffer(BufferTarget.ElementArrayBuffer,EBO);
-
-           GL.BufferData(BufferTarget.ArrayBuffer, Vertices.Length *
-sizeof(float), Vertices,BufferUsageHint.StaticDraw);
-           GL.BufferData(BufferTarget.ElementArrayBuffer,
-Indices.Length * sizeof(uint), Indices,BufferUsageHint.StaticDraw);
-
-           GL.VertexAttribPointer(0, 2,
-VertexAttribPointerType.Float, false, 2 * sizeof(float), 0);
-           GL.EnableVertexAttribArray(0);
-           GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-           GL.BindVertexArray(0);
-       }
-       public void Draw(Shader myShader)
-       {
-           //applying shader and creating the two triangle models, then scaling them down, and applying a uniform to the time and model
-           GL.BindVertexArray(VAO);
-           Matrix4 model = Matrix4.CreateScale(size.X, size.Y, 1);
-           model = model * Rotation(angle - (float)Math.PI/2, Vector3.UnitZ);
-
-           model = model * Matrix4.CreateTranslation(position.X, position.Y, 0);
-           
-           myShader.SetMatrix4("u_model", model);
-           myShader.SetFloat("u_time", time);
-           
-           GL.DrawElements(PrimitiveType.Triangles, 3, DrawElementsType.UnsignedInt, 0);
-
-       }
-       private void Reset()
-       {
-           //random position within the space above the window, the larger position.Y range is the more spread out the raindrops are in the Y-dir
-           position.X = (float)rnd.NextDouble() * WindowSize.X;
-           position.Y = (float)rnd.NextDouble() * WindowSize.Y;
-            angle = (float)(rnd.NextDouble() * Math.PI * 2);
-           time = (float)rnd.NextDouble();
-
-        
-       }
-       public void Update(float DeltaTime, List<Boid> boids)
-       {
-        velocity.X = maxVelocity * (float)Math.Cos(angle);
-        velocity.Y = maxVelocity * (float)Math.Sin(angle);
-
-           //using deltatime to ensure the velocity is always the same no matter the framerate
-           //implementing acceleration
-           position = position + velocity * DeltaTime;
-           time += DeltaTime;
-           //calling the position reset of the droplet
-           if (position.Y > WindowSize.Y + 10 || position.Y < -10 || position.X > WindowSize.X + 10 || position.X < -10)
-           {
-               Reset();
-           }
-           List<Boid> foundBoids = FindBoidsInRange(boids);
-           Console.WriteLine(foundBoids.Count);
-       }
-       public List<Boid> FindBoidsInRange(List<Boid> Boids)
-       {
-        List<Boid> FoundBoids = new List<Boid>(); 
-
-        if(position.Y < 0 || position.Y >= WindowSize.Y) return FoundBoids;
-
-        foreach(Boid boid in Boids)
-        {
-            
-            Vector2 BoidPosition = boid.Position;
-            if(BoidPosition == position) continue;
-            if(BoidPosition.Y < 0 || BoidPosition.Y >= WindowSize.Y) continue;
-
-            Vector2 VectorDistance = BoidPosition - Position;
-            double Distance = Math.Sqrt(VectorDistance.X * VectorDistance.X + VectorDistance.Y * VectorDistance.Y);
-            if(Distance <= VisualRange)
+            };
+            //index of the vertices for constructing triangle
+            uint[] Indices = new uint[]
             {
-                FoundBoids.Add(boid);
-            }
-        }
-        return FoundBoids;
-       }
-       public void Separation()
-       {
-        
-       }
-       public Matrix4 Rotation( float angle, Vector3 axis)
-       {
-       return Matrix4.CreateFromAxisAngle(axis, angle);
+           0,1,2,
+            };
 
-       }
-   }
+            VAO = GL.GenVertexArray();
+            int VBO = GL.GenBuffer();
+            int EBO = GL.GenBuffer();
+
+            GL.BindVertexArray(VAO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, EBO);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, Vertices.Length *
+ sizeof(float), Vertices, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ElementArrayBuffer,
+ Indices.Length * sizeof(uint), Indices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 2,
+ VertexAttribPointerType.Float, false, 2 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+        }
+        public void Draw(Shader myShader)
+        {
+            //applying shader and creating the two triangle models, then scaling them down, and applying a uniform to the time and model
+            GL.BindVertexArray(VAO);
+            Matrix4 model = Matrix4.CreateScale(size.X, size.Y, 1);
+
+            model = model * Rotation(Math.Atan2(velocity.Y, velocity.X) - (float)Math.PI / 2, Vector3.UnitZ);
+
+            model = model * Matrix4.CreateTranslation(position.X, position.Y, 0);
+
+            myShader.SetMatrix4("u_model", model);
+            myShader.SetFloat("u_time", time);
+
+            GL.DrawElements(PrimitiveType.Triangles, 3, DrawElementsType.UnsignedInt, 0);
+
+        }
+        private void Reset()
+        {
+            //random position within the space above the window, the larger position.Y range is the more spread out the raindrops are in the Y-dir
+            position.X = (float)rnd.NextDouble() * WindowSize.X;
+            position.Y = (float)rnd.NextDouble() * WindowSize.Y;
+            velocity.X = (float)(rnd.NextDouble() * 2 - 1) * maxVelocity;
+            velocity.Y = (float)(rnd.NextDouble() * 2 - 1) * maxVelocity;
+            time = (float)rnd.NextDouble();
+
+
+        }
+        public void Update(float DeltaTime, List<Boid> boids)
+        {
+
+            //using deltatime to ensure the velocity is always the same no matter the framerate
+            position = position + velocity * DeltaTime;
+            time += DeltaTime;
+            if (position.Y > WindowSize.Y + 10)
+                position.Y = -10;
+            else if (position.Y < -10)
+                position.Y = WindowSize.Y + 10;
+            if (position.X > WindowSize.X + 10)
+                position.X = -10;
+            else if (position.X < -10)
+                position.X = WindowSize.X + 10;
+
+            List<Boid> foundBoids = FindBoidsInRange(boids);
+            CollectAllVectors(foundBoids);
+        }
+        public List<Boid> FindBoidsInRange(List<Boid> Boids)
+        {
+            List<Boid> FoundBoids = new List<Boid>();
+
+            if (position.Y < 0 || position.Y >= WindowSize.Y) return FoundBoids;
+
+            foreach (Boid boid in Boids)
+            {
+
+                Vector2 BoidPosition = boid.Position;
+                if (BoidPosition == position) continue;
+                if (BoidPosition.Y < 0 || BoidPosition.Y >= WindowSize.Y) continue;
+
+                Vector2 VectorDistance = BoidPosition - Position;
+                double Distance = Math.Sqrt(VectorDistance.X * VectorDistance.X + VectorDistance.Y * VectorDistance.Y);
+                if (Distance <= VisualRange)
+                {
+                    FoundBoids.Add(boid);
+                }
+            }
+            return FoundBoids;
+        }
+        public Vector2 Cohesion(List<Boid> FoundBoids)
+        {
+
+            Vector2 CentreOfMass = position;
+            foreach (Boid boid in FoundBoids)
+                CentreOfMass += boid.Position;
+
+            CentreOfMass = CentreOfMass / (FoundBoids.Count+1);
+            Vector2 SteeringCentre = (CentreOfMass - position) / 100;
+            return SteeringCentre;
+
+        }
+        public Vector2 Separation(List<Boid> FoundBoids)
+        {
+            Vector2 displacement = Vector2.Zero;
+            foreach (Boid boid in FoundBoids)
+            {
+                    Vector2 difference = position - boid.position;
+                    if (Math.Sqrt(Math.Pow(difference.X, 2.0) + Math.Pow(difference.Y, 2.0)) < SeparationInput)
+                    
+                        displacement = displacement - (boid.position - position);
+            }
+            return displacement / 100;
+        }
+        public Vector2 Alignment(List<Boid> FoundBoids)
+        {
+            Vector2 AverageDirection = Vector2.Zero;
+
+                foreach (Boid boid in FoundBoids)
+                {
+                    if (FoundBoids.Count != 0)
+                    AverageDirection += boid.velocity;
+                }
+                AverageDirection = AverageDirection / (FoundBoids.Count + 1);
+            
+
+            return AverageDirection;
+        }
+        public void CollectAllVectors(List<Boid> boids)
+        {
+            Vector2 v1 = Cohesion(boids);
+            Vector2 v2 = Separation(boids);
+            Vector2 v3 = Alignment(boids);
+            velocity = (velocity + v1 + v2 + v3);
+            velocity.Normalize();
+            velocity = velocity * maxVelocity;
+            position = (position + velocity);
+
+
+
+        }
+
+        public Matrix4 Rotation(double angle, Vector3 axis)
+        {
+            return Matrix4.CreateFromAxisAngle(axis, (float)angle);
+        }
+    }
 
 }
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -12,81 +12,80 @@ using OpenTK.Mathematics;
 
 namespace Boids
 {
-   class Window : GameWindow
-   {
-       public Window(GameWindowSettings gameWindowSettings,
-NativeWindowSettings nativeWindowSettings)
-           : base(gameWindowSettings, nativeWindowSettings)
-       { 
-        myShader = new Shader();
-        rnd = new Random();
-       }
-
-       private Shader myShader;
-       private float time;
-       private int count = 0;
-       private Random rnd;
-       private int seed = 0;
-       private List<Boid> Boids = new List<Boid>();
-       protected override void OnLoad()
-       {
-           base.OnLoad();
-           //always starts on the same seed
-           rnd = new Random(seed);
-           Boid.Setup();
-           myShader = new Shader("Shaders/VertexShader.glsl",
-"Shaders/FragmentShader.glsl");
-           for (int i = 0; i < 100; i++)
-           {
-                Boids.Add(new Boid(new Vector2(800, 800), rnd));
-           }
-       }
-
-       protected override void OnRenderFrame(FrameEventArgs args)
-       {
-           base.OnRenderFrame(args);
-           count++;
-
-           myShader.Bind();
-           time += (float)args.Time;
-           myShader.SetFloat("u_time", time);
-           Matrix4 projection = Matrix4.CreateOrthographicOffCenter(0,800, 800, 0, 0.01f, 100);
-           myShader.SetMatrix4("u_projection", projection);
-           
-           GL.ClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-           GL.Clear(ClearBufferMask.ColorBufferBit);
-           for (int i = 0; i < Boids.Count; i++)
-           {
-               Boids[i].Update((float)(args.Time), Boids);
-               Boids[i].Draw(myShader);
-           }
-
-           SwapBuffers();      
-       }
-       private float GetPosition()
-       {
-           return (float)(rnd.NextDouble() * 800);
-       }
-       protected override void OnUpdateFrame(FrameEventArgs args)
-       {
-           base.OnUpdateFrame(args);
-           KeyboardState input = KeyboardState;
-           if (input.IsKeyDown(Keys.Escape))
+    class Window : GameWindow
     {
-        Close(); 
+        public Window(GameWindowSettings gameWindowSettings,
+ NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+            myShader = new Shader();
+            rnd = new Random();
+        }
+
+        private Shader myShader;
+        private float time;
+        private int count = 0;
+        private Random rnd;
+        private int seed = 0;
+        private List<Boid> Boids = new List<Boid>();
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            //always starts on the same seed
+            rnd = new Random(seed);
+            Boid.Setup();
+            myShader = new Shader("Shaders/VertexShader.glsl", "Shaders/FragmentShader.glsl");
+            for (int i = 0; i < 100; i++)
+            {
+                Boids.Add(new Boid(new Vector2(800, 800), rnd));
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            count++;
+
+            myShader.Bind();
+            time += (float)args.Time;
+            myShader.SetFloat("u_time", time);
+            Matrix4 projection = Matrix4.CreateOrthographicOffCenter(0, 800, 800, 0, 0.01f, 100);
+            myShader.SetMatrix4("u_projection", projection);
+
+            GL.ClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            for (int i = 0; i < Boids.Count; i++)
+            {
+                Boids[i].Update((float)(args.Time), Boids);
+                Boids[i].Draw(myShader);
+            }
+
+            SwapBuffers();
+        }
+        private float GetPosition()
+        {
+            return (float)(rnd.NextDouble() * 800);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            KeyboardState input = KeyboardState;
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnResize(ResizeEventArgs args)
+        {
+            base.OnResize(args);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
     }
-       }  
-
-       protected override void OnResize(ResizeEventArgs args)
-       {
-           base.OnResize(args);
-
-           GL.Viewport(0, 0, Size.X, Size.Y);
-       }
-
-       protected override void OnUnload()
-       {
-           base.OnUnload();
-       }
-   }
 }
